f4a9dfa44fb47093c88c662bc1f12f15cac3ead7
sophie482 <83037134+sophie482@users.noreply.github.com>
2022-01-17 13:17:30 -0700

    Update bouncingBalls.py

--- bouncingBalls.py
+++ bouncingBalls.py
@@ -3,76 +3,85 @@ import random
 
 width = 600
 height = 600
-balls = []
 
 class Ball:
     def __init__(self):
+        self.colour = None
+        self.size = 0
         self.x = 0
         self.y = 0
         self.dx = 0
         self.dy = 0
-        self.size = 0
-        self.colour = None
 
-    def makeBall():
-        ball = Ball()
-        ball.size = random.randint(10, 20)
+def createBall():
+    ball = Ball()
 
-        ball.x = random.randint(ball.size, width - ball.size)
-        ball.y = random.randint(ball.size, height - ball.size)
+    ball.colour = ((random.randint(0, 255)), (random.randint(0, 255)), random.randint(0, 255))
 
-        ball.dx = random.randint(-2, 3)
-        ball.dy = random.randint(-2, 3)
+    ball.size = random.randint(10, 20)
 
-        ball.colour = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    ball.x = random.randint((ball.size), (width - ball.size))
+    ball.y = random.randint((ball.size), (height - ball.size))
 
-        return ball
+    ball.dx = random.randint(-5, 6)
+    ball.dy = random.randint(-5, 6)
 
-    def draw(self):
-        for ball in balls: 
+    return ball
+
+class ballGame(arcade.Window):
+
+    def __init__(self):
+        super().__init__(width, height, "Bouncing Balls!!")
+        self.balls = []
+        ball = createBall()
+        self.balls.append(ball)
+
+    # special handler function for drawing (doesn't work if it's called "drawBalls" or something like that)
+    def on_draw(self):
+        # similar to pygame.init()
+        arcade.start_render()
+
+        for ball in self.balls:
             arcade.draw_circle_filled(ball.x, ball.y, ball.size, ball.colour)
 
-    def moveBall(self):
-        for ball in balls:
+    # similar special handler function for updating objects (needs delta_time argument, doesn't work otherwise)
+    #uses physics engine
+    def on_update(self, delta_time):
+        for ball in self.balls:
             ball.x += ball.dx
             ball.y += ball.dy
 
+            # hitting walls (takes into account ball width so it doesn't half-disappear)
+            # if ball hits left side of the screen
             if ball.x < ball.size:
                 ball.dx *= -1
+
+            # if ball hits top of screen
             if ball.y < ball.size:
-                ball.dy *= -1
+                ball.dy *= - 1
+
+            # if ball hits right side of screen
             if ball.x > width - ball.size:
                 ball.dx *= -1
+            
+            # if ball hits bottom of screen
             if ball.y > height - ball.size:
                 ball.dy *= - 1
 
+     # special handler for mouse clicks (button is button that is pressed, modifiers are all keys like shift/ctrl pressed )
+    def on_mouse_press(self, x, y, button, modifiers):
+        ball = createBall()
+        self.balls.append(ball)
+
+    # almost the same as on_mouse_press but with keys instead of buttons
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.DOWN:
+            self.balls.pop()
+
 def main():
-    # width, height, title, resizable, antialiasing
-    arcade.open_window(width, height, "Bouncing Balls", False, True)
-    # start drawing (similar to pygame.init())
-    arcade.start_render()
-
-    # create/draw/move balls
-    for i in range(0, 6):
-
-        ball = Ball.makeBall()
-        balls.append(ball)
-        Ball.draw(ball)
-    
-    while True: 
-        Ball.moveBall(ball)
-
-    # stop drawing (without this the drawing flashes)
-    arcade.finish_render()
-
+    ballGame()
     arcade.run()
 
-
 main()
-
-
-    # def on_mouse_press(self):
-    #     ball = Ball.makeBall()
-    #     self.balls.append(ball)
 
 
